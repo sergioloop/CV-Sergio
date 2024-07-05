7af98cad71e5e575666672328d02405b05acbb1c
sergioloop <91091243+sergioloop@users.noreply.github.com>
2024-07-05 12:47:28 +0200

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   public/pdf/CV_En_Sergio_Lopez_Garcia.pdf
    	new file:   public/pdf/CV_Es_Sergio_Lopez_Garcia.pdf
    	new file:   public/pdf/CV_cat_Sergio_Lopez_Garcia.pdf
    	deleted:    public/pdf/Juan_A_Valdivia_Camacho_CV_CA.pdf
    	deleted:    public/pdf/Juan_A_Valdivia_Camacho_CV_EN.pdf
    	deleted:    public/pdf/Juan_A_Valdivia_Camacho_CV_ES.pdf
    	modified:   src/assets/style.css
    	modified:   src/components/CardDownload.jsx

--- src/components/CardDownload.jsx
+++ src/components/CardDownload.jsx
@@ -12,9 +12,9 @@ export default function CardDownload() {
       </button>
       <UnOrderedList addclass="hidden group-hover:absolute top-full w-40 py-8 -left-40 md:-left-20 group-hover:flex ">
         <article className="bg-white w-full  flex flex-col rounded-lg border-2 border-[#575757] overflow-hidden">
-          <PdfDownLoad name="Juan_A_Valdivia_Camacho_CV_EN" lang="EN" />
-          <PdfDownLoad name="Juan_A_Valdivia_Camacho_CV_ES" lang="ES" />
-          <PdfDownLoad name="Juan_A_Valdivia_Camacho_CV_CA" lang="CAT" />
+          <PdfDownLoad name="CV_En_Sergio_Lopez_Garcia" lang="EN" />
+          <PdfDownLoad name="CV_Es_Sergio_Lopez_Garcia" lang="ES" />
+          <PdfDownLoad name="CV_cat_Sergio_Lopez_Garcia" lang="CAT" />
         </article>
       </UnOrderedList>
     </section>
