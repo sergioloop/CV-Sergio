83385795cded17d85601a0b301d38d73dd350247
sergioloop <91091243+sergioloop@users.noreply.github.com>
2024-07-05 10:33:13 +0200

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   src/components/LinkSkill.jsx
    
    Changes not staged for commit:
    	modified:   src/components/LinkSkill.jsx

---
+++ src/components/LinkSkill.jsx
@@ -0,0 +1,21 @@
+export default function LinkSkill({ data }) {
+    return (
+        <a
+            target="_blank"
+            href={data.href}
+            title={data.name}
+            key={data.id}
+            className="hover:scale-110 active:scale-95 transition-transform duration-300"
+        >
+            {data.Component({
+                fill:
+                    (data.fill && data.fill) 
+                    (data.stroke && data.stroke) 
+                    "",
+                height: "100%",
+                width: "100%",
+                src: data.href
+            })}
+        </a>
+    )
+}
